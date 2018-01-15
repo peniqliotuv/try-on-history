5f694bed1d24bf5f5c9970516ef8cd272003b776
peniqliotuv <jerrytsu@usc.edu>
2018-01-15 10:55:47 -0800

    React Native Animation

--- app/try-on-history/src/components/ListItem/index.js
+++ app/try-on-history/src/components/ListItem/index.js
@@ -1,37 +1,89 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import {
+  View,
+  TouchableOpacity,
+  Text,
+  Animated,
+  Easing,
+  StyleSheet,
+} from 'react-native';
 import PropTypes from 'prop-types';
 import styles from './styles';
 
-const ListItem = ({ history = {} }) => (
-  <View style={styles.container}>
-    <Text
-      style={styles.productName}
-      selectable={false}
-      numberOfLines={1}
-    >
-      { history.productName || '' }
-    </Text>
-    <Text style={styles.date}>
-      { (new Date(history.dateTriedOn).toLocaleDateString()) || '' }
-    </Text>
-  </View>
-);
+class ListItem extends Component {
 
-ListItem.propTypes = {
-  history: PropTypes.shape({
-    productName: PropTypes.string,
-    upc: PropTypes.string,
-    datePurchased: PropTypes.oneOfType([
-      PropTypes.object,
-      PropTypes.string,
-    ]),
-    dateTriedOn: PropTypes.oneOfType([
-      PropTypes.object,
-      PropTypes.string,
-    ]),
-    purchased: PropTypes.bool,
-  }).isRequired,
-};
+  static propTypes = {
+    history: PropTypes.shape({
+      productName: PropTypes.string,
+      upc: PropTypes.string,
+      datePurchased: PropTypes.oneOfType([
+        PropTypes.object,
+        PropTypes.string,
+      ]),
+      dateTriedOn: PropTypes.oneOfType([
+        PropTypes.object,
+        PropTypes.string,
+      ]),
+      purchased: PropTypes.bool,
+    }).isRequired,
+  };
+
+  static defaultProps = {
+    history: {},
+  };
+
+  animatedValue = new Animated.Value(0);
+  isExpanded = false;
+
+  animate = () => {
+    console.log('Animating');
+    this.isExpanded ? this.animatedValue.setValue(1) : this.animatedValue.setValue(0);
+    Animated.timing(
+      this.animatedValue,
+      {
+        toValue: this.isExpanded ? 0 : 1,
+        duration: 250,
+        easing: Easing.linear,
+      },
+    ).start(() => this.isExpanded = !this.isExpanded);
+  };
+
+  render() {
+    const containerStyles = StyleSheet.flatten([
+      styles.container,
+      {
+        paddingTop: this.animatedValue.interpolate({
+          inputRange: [0, 1],
+          outputRange: [10, 40],
+        }),
+        paddingBottom: this.animatedValue.interpolate({
+          inputRange: [0, 1],
+          outputRange: [10, 40],
+        }),
+      },
+    ]);
+    return (
+      <Animated.View
+        style={containerStyles}
+      >
+        <TouchableOpacity
+          onPress={() => this.animate()}
+          activeOpacity={0.9}
+        >
+        <Text
+          style={styles.productName}
+          selectable={false}
+          numberOfLines={1}
+        >
+          { this.props.history.productName || '' }
+        </Text>
+        <Text style={styles.date}>
+          { (new Date(this.props.history.dateTriedOn).toLocaleDateString()) || '' }
+        </Text>
+      </TouchableOpacity>
+      </Animated.View>
+    );
+  }
+}
 
 export default ListItem;
